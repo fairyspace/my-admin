a6bb876f4b1d817babf5480535292a35784cdbb9
fairyspace <krhudsong@gmail.com>
2024-11-22 13:51:12 +0800

    #C1: 第一次提交到本地

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const str = 'hello world'
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+</script>
+
+<template>
+  <div class="box">
+
+    <h1>App根组件</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: aqua;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
